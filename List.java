e72490eb840f77e57c8e2204c0bbd045509d4afa
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-07-02 14:47:14 -0400

    Create List.java

---
+++ List.java
@@ -0,0 +1,17 @@
+public interface List<E> {
+    
+    public int size();
+    
+    public boolean isEmpty();
+    
+     public E get(int i) throws IndexOutOfBoundsException;
+     
+     public E set(int i, E e) throws IndexOutOfBoundsException;
+     
+     public void add(int i, E e) throws IndexOutOfBoundsException;
+     
+     public E remove(int i) throws IndexOutOfBoundsException;
+     
+     
+    
+}
